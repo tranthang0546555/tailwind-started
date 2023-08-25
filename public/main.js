4b2e56a057eb788e3e081cefc61f7b25d367d284
thangne <tranthang0546555@gmail.com>
2023-08-25 23:12:22 +0700

    update theme and dark mode

--- public/main.js
+++ public/main.js
@@ -11,3 +11,28 @@ navCloseButton.addEventListener("click", () => {
   navMenu.classList.add("hidden");
   navButton.classList.remove("hidden");
 });
+
+let darkBtn = document.getElementById("dark-btn");
+let lightIcon = document.getElementById("light-icon");
+let darkIcon = document.getElementById("dark-icon");
+let classList = document.documentElement.classList;
+
+const setTheme = (theme) => {
+  if (theme === "dark") {
+    classList.remove("dark");
+    lightIcon.classList.add("hidden");
+    darkIcon.classList.remove("hidden");
+  } else {
+    classList.add("dark");
+    lightIcon.classList.remove("hidden");
+    darkIcon.classList.add("hidden");
+  }
+};
+
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  setTheme("dark");
+} else setTheme("light");
+
+darkBtn.addEventListener("click", () => {
+  setTheme(classList.contains("dark") ? "dark" : "light");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,5 @@ module.exports = {
     extend: {},
   },
   plugins: ["prettier-plugin-tailwindcss"],
+  darkMode: "class",
 };
